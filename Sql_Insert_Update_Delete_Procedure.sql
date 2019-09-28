d7678211bf8d870a6239b631ea61a4d8e822bda6
turkmuhendisnet <48014368+turkmuhendisnet@users.noreply.github.com>
2019-09-28 15:28:21 +0300

    Id Null Geçile bilir
    Id değeri insert işleminde primary  key olduğundan null geçilebilir yapıldı

--- Sql_Insert_Update_Delete_Procedure.sql
+++ Sql_Insert_Update_Delete_Procedure.sql
@@ -1,11 +1,11 @@
-create proc InsertUpdateDelete (@id int,@Adi varchar(250),@Soyadi varchar(250),@KayitTarihi datetime, @Islem nvarchar(20) = '')
+create proc InsertUpdateDelete (@id int=null,@Adi varchar(250),@Soyadi varchar(250),@KayitTarihi datetime, @Islem nvarchar(20) = '')
 as
 begin
 if @Islem='Ekle'
 begin
 insert into Personel (Adi,Soyadi,KayitTarihi) values(@Adi,@Soyadi,@KayitTarihi)
 end
-if @Islem='Güncelle'
+if @Islem='GÃ¼ncelle'
 begin
 update Personel set Adi=@Adi,Soyadi=@Soyadi,KayitTarihi=@KayitTarihi
 where Id=@id
