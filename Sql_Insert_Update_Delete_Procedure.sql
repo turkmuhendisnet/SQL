0ae247c647f93add47f0bda9cf61ef0e3aa63a17
turkmuhendisnet <48014368+turkmuhendisnet@users.noreply.github.com>
2019-09-28 15:55:19 +0300

    İslem Hariç Tüm Eleanlar Null Geçile Bilmesi
    @Islem hariç diğer elemanlara null geçilebilir özelliği verilerek her işlem için tüm parametreleri girme zorunluluğu ortadan kaldırılmıştır.

--- Sql_Insert_Update_Delete_Procedure.sql
+++ Sql_Insert_Update_Delete_Procedure.sql
@@ -1,4 +1,4 @@
-create proc InsertUpdateDelete (@id int=null,@Adi varchar(250),@Soyadi varchar(250),@KayitTarihi datetime, @Islem nvarchar(20) = '')
+create procedure InsertUpdateDelete (@id int=null,@Adi varchar(250)=null,@Soyadi varchar(250)=null,@KayitTarihi datetime=null, @Islem nvarchar(20) = '')
 as
 begin
 if @Islem='Ekle'
@@ -10,11 +10,12 @@ begin
 update Personel set Adi=@Adi,Soyadi=@Soyadi,KayitTarihi=@KayitTarihi
 where Id=@id
 end
+if @Islem='TümünüGetir'
+begin
+select * from Personel 
+end
 else if	@Islem='Sil'
 begin
 delete from Personel where Id=@id
 end
 end
-
-
-exec InsertUpdateDelete @id=50, @Adi='Mert',@Soyadi='Sakin',@KayitTarihi='1995-06-11 00:00:00.000',@Islem='Ekle'
